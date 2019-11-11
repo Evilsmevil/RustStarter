3c093a2c8b9d85d3c8814560507f33d99d66f95b
= <=>
2019-11-10 23:35:46 -0800

    turned it into more of a stress test

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 extern crate crossbeam;
 
 fn main() {
-    let mut v = vec![0,1,2,3,4,5,6];
+    let mut v = vec![];
+
+    for x in 0..100000000 {
+        v.push(x);
+    }
 
     let length = v.len()/2;
-    let(mut head, mut tail) = v.split_at_mut(length);
-
+    let(head, tail) = v.split_at_mut(length);
+    let(mut head_head, mut headTail) = head.split_at_mut(length/2);
+    let(mut tailHead, mut tailTail) = tail.split_at_mut(length/2);
     //let slice2 = &mut v[3..6];
     crossbeam::scope(|scope| {
-        scope.spawn(move |_| write_slice(&mut head, 2));
-        scope.spawn(move |_| write_slice(&mut tail, 100));
+        scope.spawn(move |_| write_slice(&mut head_head, 2));
+        scope.spawn(move |_| write_slice(&mut headTail, 100));
+        scope.spawn(move |_| write_slice(&mut tailHead, 2));
+        scope.spawn(move |_| write_slice(&mut tailTail, 100));
     }).expect("Something went wrong");
 
+    println!("Finished threads");
     for score in &v {
         println!("{}",score);
     }
 }
 
-fn write_slice(arr : &mut [i32], multiplier: i32)
+fn write_slice(arr : &mut [i64], multiplier: i64)
 {
     for score in arr {
-        *score *= multiplier;
+        for _x in 0..10000
+        {
+            *score += multiplier;
+            *score -= multiplier;
+        }
+
         }
 }
